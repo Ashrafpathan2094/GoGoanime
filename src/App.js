33bf6fcf7ddae764638dc9d06ebdaf38ca6c392c
Ashrafpathan2094 <minazpathan2094@gmail.com>
2023-03-05 21:48:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ResponsiveAppBar from './components/Header.js';
+
+function App() {
+  return (
+    <div className='head-wrapper'>
+      <header>
+        < ResponsiveAppBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
